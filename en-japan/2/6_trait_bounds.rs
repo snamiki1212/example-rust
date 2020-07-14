41fb05ca4dfd7492f45698480ae2dd2aba31662b
snamiki1212 <snamiki1212@gmail.com>
2020-07-14 12:42:08 +0900

    Add

--- en-japan/2/6_trait_bounds.rs
+++ en-japan/2/6_trait_bounds.rs
@@ -11,6 +11,12 @@ impl Trait for S {
 fn main (){
   let s = S;
   say_say(s);
+
+  // 同じ用にdummyがsay funcを持っていても
+  // dummy はTraitの実装ではないので
+  // トレイト境界で弾かれて、エラー
+  // let d = Dummy;
+  // say_say(d);
 }
 
 // <型パラメータ: トレイト名>
@@ -20,3 +26,12 @@ fn say_say<T: Trait>(s: T){
   s.say();
   s.say();
 }
+
+
+// struct Dummy;
+// impl Dummy {
+//   fn say(&self){
+//     println!("it's dummy");
+//   }
+// }
+
