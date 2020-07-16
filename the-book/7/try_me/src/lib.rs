bcce4ea1428c4da0c657bc4ace1cab5cf49d326b
snamiki1212 <snamiki1212@gmail.com>
2020-07-17 01:15:56 +0900

    Update

---
+++ the-book/7/try_me/src/lib.rs
@@ -0,0 +1,28 @@
+mod outermost {
+    pub fn middle_function() {}
+
+    fn middle_secret_function() {}
+
+    mod inside {
+        pub fn inner_function() {
+            
+        }
+
+        fn secret_function() {}
+    }
+}
+
+fn try_me() {
+    outermost::middle_function(); // OK
+    outermost::middle_secret_function(); // NG: not pub
+    outermost::inside::inner_function();
+    outermost::inside::secret_function();
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
