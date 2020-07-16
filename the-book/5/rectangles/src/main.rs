db888f176c5a340242788de0a36d78973c088f31
snamiki1212 <snamiki1212@gmail.com>
2020-07-16 22:53:02 +0900

    Update

--- the-book/5/rectangles/src/main.rs
+++ the-book/5/rectangles/src/main.rs
@@ -1,17 +1,24 @@
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
 
-    println!(
-        "the are of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
+    println!("can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
+// #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
-fn area(dimensions: &Rectangle) -> u32 {
-    dimensions.width * dimensions.height
+impl Rectangle {
+    // fn area(&self) -> u32 {
+    //     &self.width * &self.height
+    // }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
