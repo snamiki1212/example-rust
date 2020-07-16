c468bbfd7289e5608fd5997c94c3e427071ad49d
snamiki1212 <snamiki1212@gmail.com>
2020-07-16 23:26:15 +0900

    Update

--- the-book/6/ip/src/main.rs
+++ the-book/6/ip/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
 
     println!("home is {:?} / loopback is loopbac {:?}", home, loopback);
@@ -7,6 +7,6 @@ fn main() {
 
 #[derive(Debug)]
 enum IpAddr {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String)
 }
