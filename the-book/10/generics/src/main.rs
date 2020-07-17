1a16399d62a88932e936aebed617c35f19fb0bc5
snamiki1212 <snamiki1212@gmail.com>
2020-07-17 22:14:54 +0900

    Update

--- the-book/10/generics/src/main.rs
+++ the-book/10/generics/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let list = vec![34, 50, 25, 100, 65];
+
+    let mut largest = list[0];
+    
+    for number in list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("largest: {}", largest);
 }
