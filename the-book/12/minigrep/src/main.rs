693b1b6e4e7ef164dd9c471c617a48968641e7d7
snamiki1212 <snamiki1212@gmail.com>
2020-07-18 14:27:09 +0900

    Update

--- the-book/12/minigrep/src/main.rs
+++ the-book/12/minigrep/src/main.rs
@@ -1,9 +1,8 @@
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
-use std::error::Error;
-
 use std::process;
+
+mod lib;
+use lib::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,33 +16,9 @@ fn main() {
     println!("in file {}", config.filename);
     // --
 
-    if let Err(e) = run(config) {
+    if let Err(e) = lib::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let mut f = File::open(config.filename)?;
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-    println!("Write text: \n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
