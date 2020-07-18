a6d896ac105a64168d732f6ed07e855e316e6e07
snamiki1212 <snamiki1212@gmail.com>
2020-07-18 16:45:08 +0900

    Update

--- the-book/13/closure/src/main.rs
+++ the-book/13/closure/src/main.rs
@@ -9,21 +9,21 @@ use std::thread;
 use std::time::Duration;
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num| {
+    let mut expensive_closure = Cacher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            expensive_closure(intensity)
+            expensive_closure.value(intensity)
         );
 
         println!(
             "Next, do {} situps!",
-            expensive_closure(intensity)
+            expensive_closure.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -31,8 +31,36 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} miutes!",
-                expensive_closure(intensity)
+                expensive_closure.value(intensity)
             );
         }
     }
 }
+
+
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+    {
+        calculation: T,
+        value: Option<u32>,
+    }
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+    {
+        fn new(calculation: T) -> Cacher<T> {
+            Cacher {
+                calculation,
+                value: None,
+            }
+        }
+        fn value(&mut self, arg: u32) -> u32 {
+            match self.value {
+                Some(v) => v,
+                None => {
+                    let v = (self.calculation)(arg);
+                    self.value = Some(v);
+                    v
+                }
+            }
+        }
+    }
