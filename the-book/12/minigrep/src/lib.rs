a47df5b7db4332f89e33d0156dfea8d914b925df
snamiki1212 <snamiki1212@gmail.com>
2020-07-18 20:47:05 +0900

    Update

--- the-book/12/minigrep/src/lib.rs
+++ the-book/12/minigrep/src/lib.rs
@@ -29,12 +29,18 @@ pub struct Config {
 }
 
 impl Config {
-  pub fn new(args: &[String]) -> Result<Config, &'static str> {
-      if args.len() < 3 {
-          return Err("not enough arguments");
-      }
-      let query = args[1].clone();
-      let filename = args[2].clone();
+  pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+      // skip first env arg value
+      args.next();
+
+      let query = match args.next() {
+        Some(arg) => arg,
+        None => return Err("Didn't get a query string."),
+      };
+      let filename = match args.next() {
+        Some(arg) => arg,
+        None => return Err("Didn't get a filename."),
+      };
 
       let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
@@ -44,6 +50,7 @@ impl Config {
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   let mut results = Vec::new();
+
   for line in contents.lines() {
     if line.contains(query) {
       results.push(line);
--- the-book/12/minigrep/src/main.rs
+++ the-book/12/minigrep/src/main.rs
@@ -5,9 +5,9 @@ mod lib;
 use lib::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("problem parsing arguments: {}", err);
         process::exit(1);
     });
