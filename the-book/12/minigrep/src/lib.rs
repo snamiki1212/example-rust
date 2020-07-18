366d89e35140f204dffd5946e5eb7ff975448069
snamiki1212 <snamiki1212@gmail.com>
2020-07-18 20:50:21 +0900

    Update

--- the-book/12/minigrep/src/lib.rs
+++ the-book/12/minigrep/src/lib.rs
@@ -49,14 +49,9 @@ impl Config {
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
-
-  for line in contents.lines() {
-    if line.contains(query) {
-      results.push(line);
-    }
-  }
-  results
+  contents.lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
