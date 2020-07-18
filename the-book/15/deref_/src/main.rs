cdd7b42b4a9ccaf1617b815f6013af332f7f67bf
snamiki1212 <snamiki1212@gmail.com>
2020-07-18 22:10:05 +0900

    Update

--- the-book/15/deref_/src/main.rs
+++ the-book/15/deref_/src/main.rs
@@ -1,7 +1,28 @@
 fn main() {
-    let x = 5;
-    let y = &x;
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+    // let x = 5;
+    // let y = MyBox::new(x);
 
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y);
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}", name);
+}
+
+use std::ops::Deref;
+struct MyBox<T>(T);
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        println!("deref!!");
+        &self.0
+    }
 }
