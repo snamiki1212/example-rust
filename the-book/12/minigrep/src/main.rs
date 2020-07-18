6b0c1cfe388c63c93d74b9a65e5e8b893c3f5b41
snamiki1212 <snamiki1212@gmail.com>
2020-07-18 13:49:21 +0900

    Update

--- the-book/12/minigrep/src/main.rs
+++ the-book/12/minigrep/src/main.rs
@@ -5,12 +5,12 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
-    println!("Searching for {}", query);
-    println!("in file {}", filename);
+    let config = parse_config(&args);
+    println!("Searching for {}", config.query);
+    println!("in file {}", config.filename);
     // --
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
 
@@ -18,10 +18,15 @@ fn main() {
     println!("Write text: \n{}", contents);
 }
 
+struct Config {
+    query: String,
+    filename: String,
+}
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
 
-    (query, filename)
+    let config = Config { query, filename };
+    config
 }
