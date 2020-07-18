a90c778ea24f6c7b0741085cd1a87dd7b1664a76
snamiki1212 <snamiki1212@gmail.com>
2020-07-18 15:38:31 +0900

    Update

--- the-book/12/minigrep/src/lib.rs
+++ the-book/12/minigrep/src/lib.rs
@@ -1,13 +1,21 @@
+use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
   let mut f = File::open(config.filename)?;
+
   let mut contents = String::new();
   f.read_to_string(&mut contents)?;
 
-  for line in search(&config.query, &contents) {
+  let results = if config.case_sensitive {
+    search(&config.query, &contents)
+  } else {
+    search_case_insensitive(&config.query, &contents)
+  };
+
+  for line in results {
     println!("{}", line);
   }
   
@@ -17,6 +25,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 pub struct Config {
   pub query: String,
   pub filename: String,
+  pub case_sensitive: bool,
 }
 
 impl Config {
@@ -27,7 +36,9 @@ impl Config {
       let query = args[1].clone();
       let filename = args[2].clone();
 
-      Ok(Config { query, filename })
+      let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+      Ok(Config { query, filename, case_sensitive })
   }
 }
 
@@ -41,7 +52,7 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   results
 }
 
-fn search_case_insentive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   let query = query.to_lowercase();
   let mut results = Vec::new();
   for line in contents.lines() {
@@ -84,7 +95,7 @@ Trust me.
     ";
     assert_eq!(
       vec!["Rust:", "Trust me."],
-      search_case_insentive(query, contents)
+      search_case_insensitive(query, contents)
     )
   }
 
