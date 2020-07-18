77686443775a45e1f516c6c53103b6fbb6eb71bf
snamiki1212 <snamiki1212@gmail.com>
2020-07-18 15:31:08 +0900

    Update

--- the-book/12/minigrep/src/lib.rs
+++ the-book/12/minigrep/src/lib.rs
@@ -41,9 +41,52 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   results
 }
 
+fn search_case_insentive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let query = query.to_lowercase();
+  let mut results = Vec::new();
+  for line in contents.lines() {
+    if line.to_lowercase().contains(&query) {
+      results.push(line);
+    }
+  }
+  results
+}
+
+
 #[cfg(test)]
 mod test {
   use super::*;
+
+  #[test]
+  fn case_sensitive() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.
+    ";
+
+    assert_eq!(
+      vec!["safe, fast, productive."],
+      search(query, contents)
+    )
+  }
+
+  #[test]
+  fn case_insentive() {
+    let query ="rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.
+    ";
+    assert_eq!(
+      vec!["Rust:", "Trust me."],
+      search_case_insentive(query, contents)
+    )
+  }
 
   #[test]
   fn one_result() {
