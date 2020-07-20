ee7239c2be64deeae8aad05a91b899a9d9092c58
snamiki1212 <snamiki1212@gmail.com>
2020-07-20 18:44:52 +0900

    Update

--- the-book/16/channel_/src/main.rs
+++ the-book/16/channel_/src/main.rs
@@ -4,7 +4,9 @@ use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
 
+    // 1st channel
     thread::spawn(move || {
         let list = vec![
             String::from("no1"),
@@ -18,6 +20,24 @@ fn main() {
             thread::sleep(Duration::from_secs(1));
         }
     });
+
+    thread::sleep(Duration::from_millis(500));
+
+    // 2nd channel
+    thread::spawn(move || {
+        let list = vec![
+            String::from("no1_2"),
+            String::from("no2_2"),
+            String::from("no3_2"),
+            String::from("no4_2"),
+            String::from("no5_2"),
+        ];
+        for item in list {
+            tx1.send(item).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
     for received in rx {
         println!("received: {}", received);
     }
