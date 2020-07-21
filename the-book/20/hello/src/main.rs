fca52f25f89c3c443faa200e709987b4951ab8f6
snamiki1212 <snamiki1212@gmail.com>
2020-07-22 00:22:13 +0900

    Update

--- the-book/20/hello/src/main.rs
+++ the-book/20/hello/src/main.rs
@@ -1,3 +1,25 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    let response = "AHTTP/1.1 s200 OK\r\n\r\n";
+
+    // println!("Request {}", String::from_utf8_lossy(&buffer[..]));
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
 }
