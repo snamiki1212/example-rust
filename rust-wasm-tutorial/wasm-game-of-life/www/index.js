36ce4346d996b102a2ae8a5a8d269e6dfe654f75
snamiki1212 <snamiki1212@gmail.com>
2020-11-12 02:36:25 +0900

    Update

--- rust-wasm-tutorial/wasm-game-of-life/www/index.js
+++ rust-wasm-tutorial/wasm-game-of-life/www/index.js
@@ -1,3 +1,11 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet("I'm Nash");
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+};
